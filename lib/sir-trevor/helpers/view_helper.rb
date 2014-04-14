d275b500ee49d68a1b1724f0382c5aad40e18be4
Vincent Lyons <vincentmlyons@gmail.com>
2014-04-14 15:25:41 +0100

    updated for rails 4

--- lib/sir-trevor/helpers/view_helper.rb
+++ lib/sir-trevor/helpers/view_helper.rb
@@ -3,48 +3,55 @@ module SirTrevor
     module ViewHelper
 
       extend ActiveSupport::Concern
-      
-      def render_sir_trevor(json, image_type = :large)
+      include Twitter::Autolink
+
+      def render_sir_trevor(json, image_type = 'large')
         if hash = parse_sir_trevor(json)
-          safe_join hash.map { |object|
+          hash.map { |object|
             render_sir_trevor_block(object, image_type)
-          }.compact
+          }.compact.join.html_safe
         else
           ''
         end
       end
 
-      def render_sir_trevor_block(object, image_type = :large)
-        view_name = "sir-trevor/blocks/#{object[:type].to_s.downcase}_block"
-
-        render(view_name,
-               :block => object[:data],
-               :image_type => image_type) if object.has_key?(:data)
+      def render_sir_trevor_snippet(json)
+        if text_item = pluck_sir_trevor_type(json, 'text')
+          sir_trevor_markdown(text_item['data']['text'])
+        else
+          ''
+        end
       end
 
-      def render_sir_trevor_image(json, image_type = :large)
-        image = pluck_sir_trevor_type(json, :image)
+      def render_sir_trevor_block(object, image_type = 'large')
+        view_name = "sir-trevor/blocks/#{object['type'].to_s.downcase}_block"
+
+        render(view_name,
+               :block => object['data'],
+               :image_type => image_type) if object.has_key?("data")
+      end
+
+      def render_sir_trevor_image(json, image_type = "large")
+        image = pluck_sir_trevor_type(json, "image")
 
         unless image.nil?
-          render(:partial => "sir-trevor/blocks/image_block",
-                 :locals => { :block => image[:data], :image_type => image_type,
-                              :protocol => request.protocol }) if image.has_key?(:data)
+          render(:partial => "sir-trevor/blocks/image_block", :locals => {:block => image['data'], :image_type => image_type, :protocol => request.protocol}) if image.has_key?("data")
         end
       end
 
       def sir_trevor_image_tag(block, image_type)
         # Does the image type exist on the block?
-        if(block[:file].present? && block[:file][image_type].present?)
+        if(block['file'].present? && block['file'][image_type].present?)
 
-          image = block[:file][image_type]
+          image = block['file'][image_type]
 
-          image_url = image[:url]
-          width = image[:width] if image[:width].present?
-          height = image[:height] if image[:height].present?
+          image_url = image['url']
+          width = image['width'] if image['width'].present?
+          height = image['height'] if image['height'].present?
 
           options = {
             :class => "sir-trevor-image #{image_type}",
-            :alt => block[:description]
+            :alt => block['description']
           }
 
           options.merge!({ :width => width }) unless width.nil?
@@ -55,26 +62,26 @@ module SirTrevor
       end
 
       def sir_trevor_markdown(text)
-        renderer = CustomMarkdownFormatter.new(:hard_wrap => true, :filter_html => true,
-                                               :autolink => true, :no_intraemphasis => true,
-                                               :fenced_code => true)
-        markdown = Redcarpet::Markdown.new(renderer)
+        options = {:hard_wrap => true, :filter_html => true, :autolink => true, :no_intraemphasis => true, :fenced_code => true}
+        renderer = Redcarpet::Render::HTML.new(options)
+        markdown = Redcarpet::Markdown.new(CustomMarkdownFormatter)
         markdown.render(text).html_safe
       end
 
       def parse_sir_trevor(json)
-        hash = (json.is_a?(String) ? SirTrevor.parse_json(json) : json)
-        return false unless hash.has_key?(:data)
-        hash[:data]
+        hash = JSON.parse(json)
+
+        return false unless hash.has_key?("data")
+        hash["data"]
       end
 
       private
         # Get's the first instance of a type from the specified JSON
         def pluck_sir_trevor_type(json, type)
-          hash = SirTrevor.parse_json(json)
-          if hash.has_key?(:data)
-            item = hash[:data].select { |item| item[:type] == type }
-            item.first[:text] if item.any?
+          hash = JSON.parse(json)
+          if hash.has_key?("data")
+            item = hash["data"].select { |item| item["type"] == type }
+            item.first
           end
         end
     end
